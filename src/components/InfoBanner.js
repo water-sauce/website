db557aa22b676dbd55cab22bd34e4a2b66db89b5
watersauce <calvin@watersauce.com>
2018-10-11 14:23:51 -0700

    minor changes

--- src/components/InfoBanner.js
+++ src/components/InfoBanner.js
@@ -8,17 +8,15 @@ function InfoBanner(props) {
              >
             <div className={`accent accent-${info.color || props.color}`}></div>
             <title-block id={index + 1}>
-                {info.title_block}``
+                {info.title_block}
             </title-block>
             <p>{info.description}</p>
         </div>
     ));
 
     return <div className="info-wrapper">
-        <div className="info-background">
-            {props.title ? <h3 className="info-grid-title">{props.title}</h3> : ""}
-            {ProdLayout}
-        </div>
+        {props.title ? <h3 className="info-grid-title">{props.title}</h3> : ""}
+        {ProdLayout}
     </div>;
 }
 
--- src/containers/WhoWeArePage.js
+++ src/containers/WhoWeArePage.js
@@ -29,11 +29,11 @@ class WhoWeArePage extends Component {
                             </Col>
                             <Col xs={12} sm={8} md={6}>
                                 <p>
-                                Headquartered in San Francisco, we are a team of bold, passionate, and idealistic thinkers who understand the ever changing environment of… have you fallen asleep yet? We don’t want to waste your time with meaningless fluff. And lets be real, we know what you’re thinking, “OK, it’s a full service design agency… but what does Water Sauce even mean?”<br/><br/>
+                                Headquartered in San Francisco, we are a team of bold, passionate, and idealistic thinkers who understand the ever changing environment of… have you fallen asleep yet? We don’t want to waste your time with meaningless fluff. And let's be real, we know what you're thinking, “OK, it's a full service design agency… but what does Water Sauce even mean?”<br/><br/>
 
                                 At Water Sauce, we recognize the core economic philosophy that all products fall into two categories: needs & wants. Needs are things one has to do in order to survive while wants are what one desires to live more comfortably. If your product is bottled water, you are in the “need” business. If you make gourmet sauce, you are in the “want” business.<br/><br/>
 
-                                We build brands that transcend these categories. Whether your product is a need or want, we want your end users to feel both. You need to stay hydrated to survive but also want that awesome brand of bottled water. You want to taste that delicious sauce but also need it- food has become too bland without it! Once your brand becomes a Water Sauce, you’re delivering something truly unforgettable to your customers.
+                                We build brands that transcend these categories. Whether your product is a need or want, we want your end users to feel both. You need to stay hydrated to survive but also want that awesome brand of bottled water. You want to taste that delicious sauce but also need it — food has become too bland without it! Once your brand becomes a Water Sauce, you're delivering something truly unforgettable to your customers.
                                 </p>
                             </Col>
                         </Row>
