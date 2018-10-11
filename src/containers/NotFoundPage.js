9919f4267d51714aaa0ddc25f86fe5700567b8f0
watersauce <calvin@watersauce.com>
2018-10-11 13:44:52 -0700

    404 page + edits

--- src/containers/NotFoundPage.js
+++ src/containers/NotFoundPage.js
@@ -1,15 +1,24 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
 
 class NotFoundPage extends Component {
     render() {
         return (
-            <div>
-                <h4>
-                    Looks like you're lost! Try trecking{" "}
-                    <Link to="/">home</Link>
-                </h4>
-            </div>
+            <section className="not-found-page">
+                <div className="not-found-haiku">
+                    <div className="">
+                        <p>A haiku to guide you:</p>
+                        <h4>
+                            <Fragment>
+                            Page not found,<br/>
+                            but we hope you find<br/>
+                            your true self
+                            </Fragment>
+                        </h4>
+                        <Link to="/">Return Home</Link>
+                    </div>
+                </div>
+            </section>
         );
     }
 }
